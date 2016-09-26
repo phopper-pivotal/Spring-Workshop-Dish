f7ebe94d57fc72a2f855f625d464fab4f9694701
phopper-pivotal <phopper@pivotal.io>
2016-09-26 12:16:34 -0600

    add initial project, built after the fact

--- labs/lab4/watching/src/main/java/io/pivotal/repositories/MovieRepository.java
+++ labs/lab4/watching/src/main/java/io/pivotal/repositories/MovieRepository.java
@@ -1,16 +1,19 @@
 package io.pivotal.repositories;
 
 import io.pivotal.domain.Movie;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
+@RepositoryRestResource(collectionResourceRel = "movies", path = "movies")
+public interface MovieRepository extends PagingAndSortingRepository<Movie, Long> {
 
-public interface MovieRepository extends CrudRepository<Movie, Long> {
+    Page<Movie> findByTitle(@Param("title") String title, Pageable pageable);
 
-    List<Movie> findByTitle(String title);
+    Page<Movie> findByRated(@Param("rated") String rated, Pageable pageable);
 
-    List<Movie> findByRated(String rated);
-
-    List<Movie> findByGenreStartsWith(String genre);
+    Page<Movie> findByGenreStartsWith(@Param("genre") String genre, Pageable pageable);
 
 }
