bd2a565bef93bfd6523ce7cb04fcdb1dbc5c00c3
phopper-pivotal <phopper@pivotal.io>
2016-09-26 13:15:23 -0600

    add col names, otherwise they get mixed up

--- labs/lab5/import.sql
+++ labs/lab5/import.sql
@@ -1,11 +1,11 @@
-INSERT INTO MOVIE VALUES (1, 'Despicable Me', '2010', 'PG', '09 Jul 2010', '95 min', 'Animation, Comedy, Family');
-INSERT INTO MOVIE VALUES (2, 'Frozen', '2013', 'PG', '27 Nov 2013', '102 min', 'Animation, Adventure, Comedy');
-INSERT INTO MOVIE VALUES (3, 'Toy Story', '1995', 'G', '22 Nov 1995', '81 min', 'Animation, Adventure, Comedy');
-INSERT INTO MOVIE VALUES (4, 'Muppets Most Wanted', '2014', 'PG', '21 Mar 2014', '107 min', 'Adventure, Comedy, Crime');
-INSERT INTO MOVIE VALUES (5, 'The Incredibles', '2004', 'PG', '05 Nov 204', '115 min', 'Animation, Action, Adventure');
-INSERT INTO MOVIE VALUES (6, 'Despicable Me 2', '2013', 'PG', '03 Jul 2013', '98 mins', 'Animation, Comedy, Family');
-INSERT INTO MOVIE VALUES (7, 'The Minions', '2014', 'N/A', '30 Oct 2014', '11 mins', 'Short, Horror, Mystery');
-INSERT INTO MOVIE VALUES (8, 'Minions', '2015', 'PG', '30 Oct 2014', '91 min', 'Short, Horror, Mystery');
+INSERT INTO MOVIE (id, title, year, rated, released, runtime, genre) VALUES (1, 'Despicable Me', '2010', 'PG', '09 Jul 2010', '95 min', 'Animation, Comedy, Family');
+INSERT INTO MOVIE (id, title, year, rated, released, runtime, genre) VALUES (2, 'Frozen', '2013', 'PG', '27 Nov 2013', '102 min', 'Animation, Adventure, Comedy');
+INSERT INTO MOVIE (id, title, year, rated, released, runtime, genre) VALUES (3, 'Toy Story', '1995', 'G', '22 Nov 1995', '81 min', 'Animation, Adventure, Comedy');
+INSERT INTO MOVIE (id, title, year, rated, released, runtime, genre) VALUES (4, 'Muppets Most Wanted', '2014', 'PG', '21 Mar 2014', '107 min', 'Adventure, Comedy, Crime');
+INSERT INTO MOVIE (id, title, year, rated, released, runtime, genre) VALUES (5, 'The Incredibles', '2004', 'PG', '05 Nov 204', '115 min', 'Animation, Action, Adventure');
+INSERT INTO MOVIE (id, title, year, rated, released, runtime, genre) VALUES (6, 'Despicable Me 2', '2013', 'PG', '03 Jul 2013', '98 mins', 'Animation, Comedy, Family');
+INSERT INTO MOVIE (id, title, year, rated, released, runtime, genre) VALUES (7, 'The Minions', '2014', 'N/A', '30 Oct 2014', '11 mins', 'Short, Horror, Mystery');
+INSERT INTO MOVIE (id, title, year, rated, released, runtime, genre) VALUES (8, 'Minions', '2015', 'PG', '30 Oct 2014', '91 min', 'Short, Horror, Mystery');
 
 
 
